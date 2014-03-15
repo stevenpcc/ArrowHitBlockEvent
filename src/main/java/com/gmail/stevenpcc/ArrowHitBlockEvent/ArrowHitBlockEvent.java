5b8ac3ccc507aa3212f3999b1f13d2bdd9ca8ae9
Steven <stevenpcc@gmail.com>
2014-03-15 13:55:25 +0000

    Changed the ArrowHitBlockEvent to extend BlockEvent

--- src/main/java/com/gmail/stevenpcc/ArrowHitBlockEvent/ArrowHitBlockEvent.java
+++ src/main/java/com/gmail/stevenpcc/ArrowHitBlockEvent/ArrowHitBlockEvent.java
@@ -2,27 +2,22 @@ package com.gmail.stevenpcc.ArrowHitBlockEvent;
 
 import org.bukkit.block.Block;
 import org.bukkit.entity.Arrow;
-import org.bukkit.event.Event;
 import org.bukkit.event.HandlerList;
+import org.bukkit.event.block.BlockEvent;
 
-public class ArrowHitBlockEvent extends Event {
+public class ArrowHitBlockEvent extends BlockEvent {
 
     private static final HandlerList handlers = new HandlerList();
 
     private Arrow arrow;
-    private Block block;
 
     public ArrowHitBlockEvent(Arrow arrow, Block block) {
+        super(block);
         this.arrow = arrow;
-        this.block = block;
     }
 
     public Arrow getArrow() {
         return arrow;
-    }
-
-    public Block getBlock() {
-        return block;
     }
 
     public HandlerList getHandlers() {
